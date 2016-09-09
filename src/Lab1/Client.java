2959303ea35f2b9ca73007ac0defad857f33d05e
Michael Palmer <michael@michaeldpalmer.com>
2016-09-08 22:19:56 -0400

    Adding exception handling

--- src/Lab1/Client.java
+++ src/Lab1/Client.java
@@ -1,9 +1,19 @@
+/**
+ * Michael Palmer
+ * 900757121
+ * CSCI 5332
+ * 9 September 2016
+ * Lab 1
+ */
 package Lab1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
@@ -14,14 +24,13 @@ public class Client {
     /**
      * Client
      *
-     * Usage: java Client.class [host] [port]
+     * Usage: java Lab1.Client [host] [port]
      *
      * @param args Arguments
      *             host - Host to connect to. Defaults to 'localhost'.
      *             port - Port to connect on. Defaults to 8080.
-     * @throws Exception Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String host = DEFAULT_HOST;
         int port = DEFAULT_PORT;
         String message;
@@ -36,38 +45,51 @@ public class Client {
             port = Integer.parseInt(args[1]);
         }
 
-        // Connect to the server
-        Socket socket = new Socket(host, port);
-        System.out.printf("Connected to %s:%d on port %d\n",
-                socket.getInetAddress().getHostAddress(), socket.getPort(), socket.getLocalPort());
+        try {
+            // Connect to the server
+            Socket socket = new Socket(host, port);
+            System.out.printf("Connected to %s:%d on port %d\n",
+                    socket.getInetAddress().getHostAddress(), socket.getPort(), socket.getLocalPort());
 
-        // Input
-        Scanner stdIn = new Scanner(System.in);
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Input
+            Scanner stdIn = new Scanner(System.in);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        // Output
-        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-        // Prompt for a message
-        System.out.print("Send message (type 'exit' to quit): ");
-
-        // Keep requesting a message until "exit" is entered
-        while (!(message = stdIn.nextLine()).equals("exit")) {
-            // Send the message
-            output.println(message);
-            System.out.println("Message sent");
-
-            // Output the response
-            System.out.printf("Message received: %s\n\n", input.readLine());
+            // Output
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 
             // Prompt for a message
             System.out.print("Send message (type 'exit' to quit): ");
-        }
 
-        // Close connections
-        stdIn.close();
-        input.close();
-        output.close();
-        socket.close();
+            // Keep requesting a message until "exit" is entered
+            while (!(message = stdIn.nextLine()).equals("exit")) {
+                // Break if a null message is received (i.e. server stopped listening)
+                if (message == null) {
+                    break;
+                }
+
+                // Send the message
+                output.println(message);
+                System.out.println("Message sent");
+
+                // Output the response
+                System.out.printf("Message received: %s\n\n", input.readLine());
+
+                // Prompt for a message
+                System.out.print("Send message (type 'exit' to quit): ");
+            }
+
+            // Close connections
+            stdIn.close();
+            input.close();
+            output.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            System.out.printf("Unknown host - '%s'\n", e.getMessage());
+        } catch (ConnectException e) {
+            System.out.printf("Connection error - %s\n", e.getMessage());
+        } catch (IOException e) {
+            System.out.printf("IO Exception - %s\n", e.getMessage());
+        }
     }
 }
--- src/Lab1/Server.java
+++ src/Lab1/Server.java
@@ -1,6 +1,14 @@
+/**
+ * Michael Palmer
+ * 900757121
+ * CSCI 5332
+ * 9 September 2016
+ * Lab 1
+ */
 package Lab1;
 
 import java.io.*;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,40 +16,56 @@ public class Server {
 
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * Server
+     *
+     * Usage: java Lab1.Server [port]
+     *
+     * @param args Arguments
+     *             port - Port to listen on. Defaults to 8080.
+     */
+    public static void main(String[] args) {
+        // Port
         int port = DEFAULT_PORT;
         if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
         }
 
-        // Bind to the port
-        ServerSocket server = new ServerSocket(port);
-        System.out.printf("Server listening on %s:%d\n",
-                server.getInetAddress().getHostAddress(), server.getLocalPort());
+        try {
+            // Bind to the port
+            ServerSocket server = new ServerSocket(port);
+            System.out.printf("Server listening on %s:%d\n",
+                    server.getInetAddress().getHostAddress(), server.getLocalPort());
 
-        // Keep listening for connections
-        while (true) {
-            // Got a connection
-            Socket client = server.accept();
-            System.out.printf("Received connection from %s:%d\n",
-                    client.getInetAddress().getHostAddress(),
-                    client.getPort());
+            // Keep listening for connections
+            while (true) {
+                // Got a connection
+                Socket client = server.accept();
+                System.out.printf("Received connection from %s:%d\n",
+                        client.getInetAddress().getHostAddress(),
+                        client.getPort());
 
-            // Create input stream reader
-            BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String line;
+                // Create input stream reader
+                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String line;
 
-            // Read the message from the client, output it, and send it back
-            PrintWriter output = new PrintWriter(client.getOutputStream(), true);
-            while ((line = input.readLine()) != null) {
-                System.out.printf("Message: %s\n", line);
-                output.println(line);
+                // Read the message from the client, output it, and send it back
+                PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+                while ((line = input.readLine()) != null) {
+                    System.out.printf("Message received: %s\n", line);
+                    output.println(line);
+                    System.out.println("Sent message back to client");
+                }
+
+                // Close connections
+                output.close();
+                input.close();
+                client.close();
             }
-
-            // Close connections
-            output.close();
-            input.close();
-            client.close();
+        } catch (BindException e) {
+            System.out.printf("Error binding socket - %s\n", e.getMessage());
+        } catch (IOException e) {
+            System.out.printf("IO Exception - %s\n", e.getMessage());
         }
     }
 }
